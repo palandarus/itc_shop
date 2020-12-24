0a22519eb69a0355756eafdeb561ac6657a54f8c
palandarus <klarkken@yandex.ru>
2020-12-24 15:26:07 +0300

    HWL1 edded entities & repositories & services of User and Role

---
+++ shop-database/src/main/java/entities/Role.java
@@ -0,0 +1,35 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role extends AbstractItem {
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
