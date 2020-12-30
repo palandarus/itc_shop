c0ef71e3e5f237560c1b39f4def1d923560a9fe0
palandarus <klarkken@yandex.ru>
2020-12-30 18:18:40 +0300

    HWL2 reformated Controllers, added Products and Categories

--- shop-admin-ui/src/main/java/ru/geekbrains/controller/ProductController.java
+++ shop-admin-ui/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -1,26 +1,27 @@
 package ru.geekbrains.controller;
 
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.entities.Category;
 import ru.geekbrains.entities.Product;
 import ru.geekbrains.exceptions.ResourceNotFoundException;
+import ru.geekbrains.services.CategoryService;
 import ru.geekbrains.services.ProductService;
-import ru.geekbrains.utils.ProductFilter;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ProductController {
 
     private ProductService productService;
+    private CategoryService categoryService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
+        this.categoryService=categoryService;
     }
 
     @GetMapping("/products")
@@ -36,7 +37,9 @@ public class ProductController {
     public String addProduct(
             Model model
     ) {
+        List<Category> categoryList=categoryService.findAll();
         model.addAttribute("product", new Product());
+        model.addAttribute("categoryList", categoryList);
         return "product_create_form";
     }
 
@@ -54,8 +57,11 @@ public class ProductController {
 
     @GetMapping("/product/edit/{id}")
     public String showProductEditForm(@PathVariable Long id, Model model) {
+
         Product p = productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product with id: " + id + " doesn't exists (for edit)"));
+        List<Category> categoryList=categoryService.findAll();
         model.addAttribute("product", p);
+        model.addAttribute("categoryList", categoryList);
         return "product";
     }
 
